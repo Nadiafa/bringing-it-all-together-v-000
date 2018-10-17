0295506685b5e84561425aab9911566905f0592f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-17 14:08:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ class Dog
   def self.create(name:, breed:)
   #   takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database
   #   returns a new dog objec
-    dog = Dog.new(name, breed)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     
     dog
