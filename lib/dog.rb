63cb4288e60e32c3e715f088a8ec9cd5be5bd7d7
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-17 13:56:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,18 +24,10 @@ class Dog
     
     DB[:conn].execute(sql)
   end
-
-  # def self.new_from_db(row)
-  #   new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
-    
-  #   new_dog
-  # end
-
-
-
-
-
-
+  
+  
+  
+  
   # def save
   #   if self.id
   #     self.update
@@ -48,6 +40,17 @@ class Dog
   #   end 
   # end
   
+  
+  
+
+
+
+
+
+
+
+  
+  
   # def update
   #   sql = <<-SQL
   #     UPDATE dogs 
@@ -59,6 +62,12 @@ class Dog
   # end
   
   
+    # def self.new_from_db(row)
+  #   new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    
+  #   new_dog
+  # end
+
   
     # def self.find_by_name(name)
   # #   returns an instance of dog that matches the name from the DB
