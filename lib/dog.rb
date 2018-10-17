81de17cb695b93272623670643305991631f81d5
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-17 14:16:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,16 +90,16 @@ class Dog
   # end
 
   
-    # def self.find_by_name(name)
-  # #   returns an instance of dog that matches the name from the DB
-  #   sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    def self.find_by_name(name)
+  #   returns an instance of dog that matches the name from the DB
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
     
-  #   DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
     
-  #   # DB[:conn].execute(sql, name).map do |row|
-  #   #     self.new_from_db(row)
-  #   # end.first
-  # end
+    # DB[:conn].execute(sql, name).map do |row|
+    #     self.new_from_db(row)
+    # end.first
+  end
   
   
   
