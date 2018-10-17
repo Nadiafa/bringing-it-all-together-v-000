cbba9b025581e594599d262560cca5b7f08760c0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-17 13:16:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :name, :breed, :id 
   def initialize(name:, breed:, id=nil)
-    @name  = name
-    @breed = breed
-    @id    = id 
+    @name  = name:
+    @breed = bree::
+    @id    = id:
   #   has a name and a breed (FAILED - 1)
   #   has an id that defaults to `nil` on initialization (FAILED - 2)
   #   accepts key value pairs as arguments to initializ
